62dcd96b2935853769c91a061eb22dab06d6f8bf
mj <148041616+yanachified@users.noreply.github.com>
2024-10-08 22:23:13 +0800

    update notification screen

--- app/screens/NotificationScreen.js
+++ app/screens/NotificationScreen.js
@@ -1,146 +1,132 @@
-import React from 'react';
-import { StyleSheet, Text, View, Image, Button, Alert, Pressable, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
 import MainGradient from '../components/MainGradient';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import React, { useContext, useState, useEffect } from 'react';
+import { ApiContext } from '../../server/Api';
+import axios from 'axios';
+
 
 function NotificationScreen(props) {
-    return (
-        <SafeAreaView style={styles.container}>
-            <MainGradient/>
-            <View>
-                <Text style={styles.headerText}>{props.route.name}</Text>
-            </View>
-            <View style={styles.body}>
-                <View style={styles.card}>
-                    <Text style={styles.cardTitle}>My First Fish Tank</Text>
-                    <Text style={styles.markText}>Mark all as read</Text>
-                    <ScrollView style={{marginBottom: 120}}>
-                        <View style={styles.cardBody}>
-                            <View>
-                                <Text style={styles.recentText}>Successful Feeding - 08:00 AM</Text>
-                            </View>
-                            <View>
-                                <Text style={styles.recentSubText}>2 hours ago</Text>
-                            </View>
-                        </View>
-                        <View style={styles.cardBody}>
-                            <View>
-                                <Text style={styles.recentText}>Successful Feeding - 08:00 AM</Text>
-                            </View>
-                            <View>
-                                <Text style={styles.recentSubText}>2 hours ago</Text>
-                            </View>
-                        </View>
-                        <View style={styles.cardBody}>
-                            <View>
-                                <Text style={styles.recentText}>Successful Feeding - 08:00 AM</Text>
-                            </View>
-                            <View>
-                                <Text style={styles.recentSubText}>2 hours ago</Text>
-                            </View>
-                        </View>
-                        <View style={styles.cardBody}>
-                            <View>
-                                <Text style={styles.recentText}>Successful Feeding - 08:00 AM</Text>
-                            </View>
-                            <View>
-                                <Text style={styles.recentSubText}>2 hours ago</Text>
-                            </View>
-                        </View>
-                        <View style={styles.cardBody}>
-                            <View>
-                                <Text style={styles.recentText}>Successful Feeding - 08:00 AM</Text>
-                            </View>
-                            <View>
-                                <Text style={styles.recentSubText}>2 hours ago</Text>
-                            </View>
-                        </View>
-                        <View style={styles.cardBody}>
-                            <View>
-                                <Text style={styles.recentText}>Successful Feeding - 08:00 AM</Text>
-                            </View>
-                            <View>
-                                <Text style={styles.recentSubText}>2 hours ago</Text>
-                            </View>
-                        </View>
-                        <View style={styles.cardBody}>
-                            <View>
-                                <Text style={styles.recentText}>Successful Feeding - 08:00 AM</Text>
-                            </View>
-                            <View>
-                                <Text style={styles.recentSubText}>2 hours ago</Text>
-                            </View>
-                        </View>
-                        <View style={styles.cardBody}>
-                            <View>
-                                <Text style={styles.recentText}>Successful Feeding - 08:00 AM</Text>
-                            </View>
-                            <View>
-                                <Text style={styles.recentSubText}>2 hours ago</Text>
-                            </View>
-                        </View>
-                        <View style={styles.cardBody}>
-                            <View>
-                                <Text style={styles.recentText}>Successful Feeding - 08:00 AM</Text>
-                            </View>
-                            <View>
-                                <Text style={styles.recentSubText}>2 hours ago</Text>
-                            </View>
-                        </View>
-                        <View style={styles.cardBody}>
-                            <View>
-                                <Text style={styles.recentText}>Successful Feeding - 08:00 AM</Text>
-                            </View>
-                            <View>
-                                <Text style={styles.recentSubText}>2 hours ago</Text>
-                            </View>
-                        </View>
-                    </ScrollView>
+  //const context = useContext(ApiContext);
+  const [notifications, setNotifications] = useState([]);
+  const instance = axios.create({
+        baseURL: 'http://192.168.1.4:8000/'
+
+    });
+
+  const getNotifications = async () => {
+      try {
+          const response = await instance.get('notification');
+          setNotifications(response.data); 
+      } catch (error) {
+          console.error("Error fetching notifications:", error);
+      }
+  };
+
+  useEffect(() => {
+      getNotifications(); 
+  }, []);
+
+  
+    
+  const formatDateTime = (timestamp) => {
+    const date = new Date(timestamp);
+    const formattedDate = date.toLocaleDateString(); 
+    const formattedTime = date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }); 
+    return `${formattedDate} ${formattedTime}`;
+  };
+
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <MainGradient />
+      <View>
+        <Text style={styles.headerText}>Notifications</Text>
+      </View>
+      <View style={styles.body}>
+        <View style={styles.card}>
+          <Text style={styles.cardTitle}>My First Fish Tank</Text>
+          <Text style={styles.markText}>Mark all as read</Text>
+          
+          
+          <ScrollView contentContainerStyle={{ paddingBottom: 100 }}>
+            <View style={styles.cardBody}>
+              {notifications.map((item, index) => (
+                <View key={index} style={styles.notificationCard}>
+                 
+                  <Text style={styles.recentText}>{item.message}</Text>
+                 
+                  <Text style={styles.timestampText}>{formatDateTime(item.created_at)}</Text>
                 </View>
+              ))}
             </View>
-        </SafeAreaView>
-    );
+          </ScrollView>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        paddingHorizontal: 36
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    paddingHorizontal: 36,
+  },
+  headerText: {
+    fontSize: 40,
+    fontWeight: 'bold',
+    paddingTop: 40,
+  },
+  body: {
+    flex: 1,
+    marginTop: 20,
     },
-    headerText: {
-        fontSize: 40,
-        fontWeight: "bold",
-        paddingTop: 40
-    },
-    body: {
-        marginVertical: 60,
-    },
-    card: {
+  card: {
         marginBottom: 32
     },
-    cardTitle: {
-        fontSize: 28,
-        fontWeight: "bold"
+  cardTitle: {
+    fontSize: 28,
+    fontWeight: "bold"
+},
+recentText: {
+    fontSize: 20,
+    fontWeight: "bold"
     },
-    cardSubTitle: {
-        fontSize: 16,
-        color: "grey"
+markText: {
+    fontSize: 14,
+    color: '#007bff',
+    marginBottom: 20,
     },
-    cardBody: {
-        borderWidth: 1,
-        borderRadius: 10,
-        marginVertical: 3,
-        paddingVertical: 10,
-        paddingHorizontal: 15
+timestampText: {
+    fontSize: 16,
+    color: "grey"
     },
-    recentText: {
-        fontSize: 24,
-        fontWeight: "bold"
-    },
-    recentSubText: {
-        fontSize: 16,
-        color: "grey"
+ 
+  notificationCard: {
+    backgroundColor: '#fff',
+    padding: 15,
+    marginBottom: 10,
+    borderRadius: 10,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 5,
+    elevation: 3,
+    borderWidth: 1,
+    borderRadius: 10,
+    marginVertical: 3,
+    paddingVertical: 10,
+    paddingHorizontal: 15
+  },
+  messageText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  timestampText: {
+    fontSize: 14,
+    color: 'grey',
+    marginTop: 5,
     },
     buttonMargin: {
         marginVertical: 2
@@ -153,7 +139,7 @@ const styles = StyleSheet.create({
       alignItems: "center",
       borderRadius: 10,
     },
-    markText: {
+  markText: {
         textAlign: "right",
         marginVertical: 5,
         color: "#0E79B4"
