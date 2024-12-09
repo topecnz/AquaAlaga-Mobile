560c133060788371082bd7360deed2edd26b612a
topecnz <topecnz@gmail.com>
2024-12-09 16:25:33 +0800

    remove unused stack navigation

--- app/navigations/StackNav.js
+++ app/navigations/StackNav.js
@@ -4,12 +4,9 @@ import { createStackNavigator } from '@react-navigation/stack';
 import LandingScreen from '../screens/LandingScreen';
 import LoginScreen from '../screens/LoginScreen';
 import BottomTab from './BottomTab';
-import DeviceScreen from '../screens/DeviceScreen';
 import BottomTabMain from './BottomTabMain';
-import AddDeviceScreen from '../screens/AddDeviceScreen';
 import LinkDeviceScreen from '../screens/LinkDeviceScreen';
 import FirstSetupScreen from '../screens/FirstSetupScreen';
-import ResetPasswordScreen from '../screens/ResetPasswordScreen';
 import ScanWifiScreen from '../screens/ScanWifiScreen';
 import SignUpScreen from '../screens/SignUpScreen';
 import VerificationScreen from '../screens/VerificationScreen';
@@ -29,7 +26,6 @@ function StackNav(props) {
             <Stack.Screen name="BottomTabMain" component={BottomTabMain} />
             <Stack.Screen name="First Setup" component={FirstSetupScreen} />
             <Stack.Screen name="BottomTab" component={BottomTab} />
-            <Stack.Screen name="Add Device" component={AddDeviceScreen} />
             <Stack.Screen name="Connect to Wifi" component={ScanWifiScreen} />
             <Stack.Screen name="Link Device" component={LinkDeviceScreen} />
             <Stack.Screen name="Reset Password" component={ResetScreen} />
